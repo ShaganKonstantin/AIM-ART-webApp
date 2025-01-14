807c17cf4be37041c8cb98fe63beea963cada526
Konstantin Shagan <konstantinshagan@gmail.com>
2025-01-14 15:20:52 +0300

    Created button & header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp;
 
 function App() {
 //useEffect используется для работы с внешнем апи и вызывается на самом верху компонента
   useEffect(() => {
     tg.ready();
   }, [])
-
-  const onClose = () => {
-    tg.close()
-  }
 
   return (
     <div className="App">
